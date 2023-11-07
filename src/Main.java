19cfa8345cefd50749b0f43e20c76dec5dc0c76f
Plamadeala Max <linkolnmax24@gmail.com>
2023-11-07 15:24:19 +0200

    start lab4

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,12 @@
-import file.FileManager;
-import models.Faculty;
-import menu.DisplayMenu;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
-    private static final ArrayList<Faculty> faculties = FileManager.loadData();
+
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char choice;
 
-        do {
-            choice = DisplayMenu.displayMainMenu(scanner);
-
-            switch (choice) {
-                case 'g':
-                    menu.GeneralOperationsMenu.doGeneralOperationsMenu(scanner, faculties);
-                    break;
-                case 'f':
-                    menu.FacultyOperationsMenu.doFacultyOperationsMenu(scanner, faculties);
-                    break;
-                case 's':
-                    System.out.println("Sorry, not implemented!");
-                    break;
-                case 'q':
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid input. Please try again.");
-            }
-
-        } while (choice != 'q');
-
-        scanner.close();
     }
 }
